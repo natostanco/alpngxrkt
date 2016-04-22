e2c342e8f9ce75b0189084d000c81b23778393d7
natostanco <bassobassista@gmail.com>
2016-04-22 20:12:01 +0200

    changed config to mount volume

---
+++ acb2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+shopt -s expand_aliases
+source /home/dev/.aliases
+acbuild --debug begin ./alpine.aci
+acbuild --debug set-name natostanco/alpngxrkt
+acbuild --debug run -- /bin/sh -c "apk add --no-cache --update openssl nginx apr apr-util libjpeg-turbo icu pcre gnupg jq"
+acbuild --debug run -- wget `curl -s https://api.github.com/repos/natostanco/ngxpgs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4` -O ngx.tar.gz
+acbuild --debug run -- /bin/sh -c "tar -zxf ngx.tar.gz && rm ngx.tar.gz && mv nginx /usr/sbin/nginx && mv libpng* /usr/lib && rm -rf /etc/nginx"
+acbuild --debug run -- /bin/sh -c "apk del gnupg jq"
+acbuild --debug run -- /bin/sh -c "mkdir /etc/nginx"
+acbuild --debug mount add etc-nginx /etc/nginx
+
+#adduser -G www-data www-data -D
+acbuild --debug run -- /bin/sh -c "adduser -G www-data www-data -D"
+acbuild --debug port add http tcp 80
+acbuild --debug port add https tcp 443
+ 
+acbuild --debug set-exec -- /usr/sbin/nginx -g "daemon off;"
+acbuild --debug write --overwrite alpngxrkt.aci
+
+
+
