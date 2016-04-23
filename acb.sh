5c80878775b14f875472f68f56d495cb3c43ebd9
Francesco Giannelli <bassobassista@gmail.com>
2016-04-23 13:55:42 +0200

    Update acb.sh

--- acb.sh
+++ acb.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-shopt -s expand_aliases
-source /home/dev/.aliases
+#shopt -s expand_aliases
+#source ~/.aliases
+alias acbuild="/home/dev/acbuild/acbuild'
+alias sudo='sudo ' 
+alias ae='sudo acbuild end'
+
 acbuild --debug begin ./alpine.aci
 acbuild --debug set-name natostanco/alpngxrkt
-acbuild --debug run -- /bin/sh -c "apk add --no-cache --update openssl nginx apr apr-util libjpeg-turbo icu pcre gnupg jq"
+acbuild --debug run -- /bin/sh -c "apk --no-cache --update upgrade && apk add --no-cache openssl nginx apr apr-util libjpeg-turbo icu pcre gnupg jq"
 acbuild --debug run -- wget `curl -s https://api.github.com/repos/natostanco/alpngxpgs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4` -O ngx.tar.gz
 acbuild --debug run -- /bin/sh -c "tar -zxf ngx.tar.gz && rm ngx.tar.gz && mv nginx /usr/sbin/nginx && mv libpng* /usr/lib && rm -rf /etc/nginx"
 acbuild --debug run -- /bin/sh -c "apk del gnupg jq"
