56d25a739aa0b80434d9021a4262b388e4acf6d0
Francesco Giannelli <bassobassista@gmail.com>
2016-04-23 16:04:38 +0200

    Update acb.sh

--- acb.sh
+++ acb.sh
@@ -5,15 +5,13 @@ alias acbuild='/home/save/acbuild'
 alias sudo='sudo ' 
 alias ae='sudo acbuild end'
 
-apt-get update && apt-get install wget curl systemd-container sudo -y -q
+apt-get update && apt-get install wget curl sudo -y -q
 cd /home/save/
 acbuild begin /home/save/alpine.aci
 acbuild set-name natostanco/alpngxrkt
-acbuild run -- /bin/sh -c "apk --no-cache --update upgrade && apk add --no-cache openssl nginx apr apr-util libjpeg-turbo icu pcre gnupg jq"
+acbuild run -- /bin/sh -c "apk --no-cache --update upgrade && apk add --no-cache openssl nginx apr apr-util libjpeg-turbo icu pcre"
 acbuild run -- wget `curl -s https://api.github.com/repos/natostanco/alpngxpgs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4` -O ngx.tar.gz
 acbuild run -- /bin/sh -c "tar -zxf ngx.tar.gz && rm ngx.tar.gz && mv nginx /usr/sbin/nginx && mv libpng* /usr/lib && rm -rf /etc/nginx"
-acbuild run -- /bin/sh -c "apk del gnupg jq"
-acbuild run -- /bin/sh -c "mkdir /etc/nginx"
 acbuild mount add etc-nginx /etc/nginx
 
 #adduser -G www-data www-data -D
@@ -22,7 +20,7 @@ acbuild port add http tcp 80
 acbuild port add https tcp 443
  
 acbuild set-exec -- /usr/sbin/nginx
-acbuild write --overwrite /home/save/alpngxrkt.aci
+acbuild write --overwrite /home/save/alpngxrkt.aci || return 1
 
 
 
