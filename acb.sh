32745063972177fc083facf8962adaea05ba6f5f
Francesco Giannelli <bassobassista@gmail.com>
2016-04-23 14:58:52 +0200

    Update acb.sh

--- acb.sh
+++ acb.sh
@@ -5,22 +5,22 @@ alias acbuild='./acbuild'
 alias sudo='sudo ' 
 alias ae='sudo acbuild end'
 
-acbuild --debug begin ./alpine.aci
-acbuild --debug set-name natostanco/alpngxrkt
-acbuild --debug run -- /bin/sh -c "apk --no-cache --update upgrade && apk add --no-cache openssl nginx apr apr-util libjpeg-turbo icu pcre gnupg jq"
-acbuild --debug run -- wget `curl -s https://api.github.com/repos/natostanco/alpngxpgs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4` -O ngx.tar.gz
-acbuild --debug run -- /bin/sh -c "tar -zxf ngx.tar.gz && rm ngx.tar.gz && mv nginx /usr/sbin/nginx && mv libpng* /usr/lib && rm -rf /etc/nginx"
-acbuild --debug run -- /bin/sh -c "apk del gnupg jq"
-acbuild --debug run -- /bin/sh -c "mkdir /etc/nginx"
-acbuild --debug mount add etc-nginx /etc/nginx
+acbuild begin ./alpine.aci
+acbuild set-name natostanco/alpngxrkt
+acbuild run -- /bin/sh -c "apk --no-cache --update upgrade && apk add --no-cache openssl nginx apr apr-util libjpeg-turbo icu pcre gnupg jq"
+acbuild run -- wget `curl -s https://api.github.com/repos/natostanco/alpngxpgs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4` -O ngx.tar.gz
+acbuild run -- /bin/sh -c "tar -zxf ngx.tar.gz && rm ngx.tar.gz && mv nginx /usr/sbin/nginx && mv libpng* /usr/lib && rm -rf /etc/nginx"
+acbuild run -- /bin/sh -c "apk del gnupg jq"
+acbuild run -- /bin/sh -c "mkdir /etc/nginx"
+acbuild mount add etc-nginx /etc/nginx
 
 #adduser -G www-data www-data -D
-acbuild --debug run -- /bin/sh -c "adduser -G www-data www-data -D"
-acbuild --debug port add http tcp 80
-acbuild --debug port add https tcp 443
+acbuild run -- /bin/sh -c "adduser -G www-data www-data -D"
+acbuild port add http tcp 80
+acbuild port add https tcp 443
  
-acbuild --debug set-exec -- /usr/sbin/nginx
-acbuild --debug write --overwrite alpngxrkt.aci
+acbuild set-exec -- /usr/sbin/nginx
+acbuild write --overwrite alpngxrkt.aci
 
 
 
